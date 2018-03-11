77ffb9ef00f9efd5e28664c26decf108119af73d
Collin McKinney <collinmmckinney@gmail.com>
2018-03-11 11:21:25 -0400

    Initial Commit

---
+++ App.js
@@ -0,0 +1,78 @@
+/**
+* Sample React Native App
+* https://github.com/facebook/react-native
+* @flow
+*/
+
+import React, { Component } from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import { imageData } from './img.js';
+
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+    componentWillMount() {
+        fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBp7nzEUBd17o1PphUD1aP9d4nAkY961Co', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                requests: [{
+                    image: {
+                        content: imageData
+                    },
+                    features: [{ type: 'TEXT_DETECTION' }]
+                }]
+            })
+        }).then(response => response.json())
+        .then(responseJson => console.log(responseJson));
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit App.js
+                </Text>
+                <Text style={styles.instructions}>
+                    {instructions}
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
